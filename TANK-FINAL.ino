ea4f0df3179bd52ea2436b665edc49f5d2a72eca
Preston <percussionist.presto@gmail.com>
2014-07-24 08:06:12 -0400

    Final Adjustments
    commented, edited spacing
    
    ALMOST DONE?!

--- TANK-FINAL.ino
+++ TANK-FINAL.ino
@@ -2,9 +2,10 @@
 #include <Ethernet.h>
 #include <RobotOpen.h>
 
-
 /* I/O Setup */
 USBJoystick usb1('0');  // Assign the logitech USBJoystick object to bundle 0
+
+/* Global Variables */
 uint16_t LightingMode = 0;
 int analogPinOut = 8;
 
@@ -23,40 +24,52 @@ void setup()
  */
 void enabled() {
   
+  /* Variable Library */
   int LFINAL=0;
   int RFINAL=0;
-  
   int LMID=0;
   int RMID=0;
   
+  // Initial reading of Joysticks
   int LEFTS = usb1.makePWM(ANALOG_LEFTY, INVERT) - 127;
   int RITES = usb1.makePWM(ANALOG_RIGHTY, NORMAL) - 127;
  
+  // Ternarys disregard slight bumps or deadzone interference
   int smthL = (LEFTS >= 8 || LEFTS <= -8) ? LEFTS : 0;
   int smthR = (RITES >= 8 || RITES <= -8) ? RITES : 0;
   
+  // Turbo Button Logic
+  // Checks for button presses
+  // Case for both buttons  
   if(((usb1.getBtn(BTN6, NORMAL)) == 1)&&((usb1.getBtn(BTN5, NORMAL))==1))
   {
+    // Sets full values for maximum possible power
     LMID = smthL;
     RMID = smthR;
     LightingMode = 250;
   }
+  
+  // Case for right button
   else if((usb1.getBtn(BTN6, NORMAL)) == 1)
   {
+    // Sets values for midspeed power
     LMID = smthL*.7;
     RMID = smthR*.7;
     LightingMode = 200;
   }
   else
   {
+    // Sets values for Low power slower movement
     LMID = smthL*.35;
     RMID = smthR*.35;
     LightingMode = 150;
   }
   
+  // Secondary smoothing for redundancy reasons
   LFINAL = (LMID >= 8 || LMID <= -8) ? LMID : 0;
   RFINAL = (RMID >= 8 || RMID <= -8) ? RMID : 0;
   
+  // Pushing final values to sidecar PWM Digital IOs
   RobotOpen.setPWM(SIDECAR_PWM1, LFINAL + 130);
   RobotOpen.setPWM(SIDECAR_PWM2, LFINAL + 130);
   RobotOpen.setPWM(SIDECAR_PWM3, RFINAL + 128);
@@ -71,7 +84,7 @@ void enabled() {
 void disabled() {
   // PWMs are automatically disabled
   
-  /*Set LightingMode*/
+  /*Set Lighting*/
   LightingMode = 100;
 }
 
@@ -82,19 +95,21 @@ void disabled() {
  * Specify a bundle ID with a single character (a-z, A-Z, 0-9) - Just make sure not to use the same twice!
  */
 void timedtasks() {
-  RobotOpen.publishAnalog(ANALOG0, 'A:');   // Bundle A
+  
+  //Publish DS Information
+  RobotOpen.publishAnalog(ANALOG0, 'A:');        // Bundle A
   RobotOpen.publishDigital(SIDECAR_PWM1, '1');   // Bundle Sidecar 1
   RobotOpen.publishDigital(SIDECAR_PWM2, '2');   // Bundle Sidecar 2
   RobotOpen.publishDigital(SIDECAR_PWM3, '3');   // Bundle Sidecar 3
   RobotOpen.publishDigital(SIDECAR_PWM4, '4');   // Bundle Sidecar 4
   
-  /*Write Lighting Mode to analog out pin*/
+  /*Write Lighting Mode*/
   analogWrite(analogPinOut, LightingMode);
 }
 
 
 /* This is the main program loop that keeps comms operational
- * There's no need to touch anything here!!!
+ * DO NOT TOUCH UNDER ANY CIRCUMSTANCES
  */
 void loop() {
   RobotOpen.pollDS();
